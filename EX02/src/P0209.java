3f34670e9ae6b84141d0c540a3134a643789e325
yongjun98 <116868167+yongjun98@users.noreply.github.com>
2023-07-17 16:25:00 +0900

    Update P0209.java

--- EX02/src/P0209.java
+++ EX02/src/P0209.java
@@ -3,10 +3,10 @@ public class P0209 {
         short a = (short)0x55ff;
         short b = (short)0x00ff;
         System.out.println("[비트연산결과]");
-        System.out.print("%04x\n",(short)(a&b));
-        System.out.print("%04x\n",(short)(a|b));
-        System.out.print("%04x\n",(short)(a^b));
-        System.out.print("%04x\n",(short)(~a));
+        System.out.printf("%04x\n",(short)(a&b));
+        System.out.printf("%04x\n",(short)(a|b));
+        System.out.printf("%04x\n",(short)(a^b));
+        System.out.printf("%04x\n",(short)(~a));
 
         byte c =20;
         byte d= -8;
@@ -14,6 +14,6 @@ public class P0209 {
         System.out.println(c<<2);
         System.out.println(c>>2);
         System.out.println(d>>2);
-        System.out.print("%x\n",(d>>>2));
+        System.out.printf("%x\n",(d>>>2));
     }
 }
