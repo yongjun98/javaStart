06246d2731593f0dda37ca3d33ca9e38b448a0c2
yongjun98 <116868167+yongjun98@users.noreply.github.com>
2023-08-15 13:23:40 +0900

    자바 5장 실습문제

---
+++ EX02/src/Color/main.java
@@ -0,0 +1,46 @@
+package Color;
+import java.util.Scanner;
+class Point {
+    private int x, y;
+    public Point(int x, int y) { this.x = x; this.y = y; }
+    public int getX() { return x; }
+    public int getY() { return y; }
+    protected void move(int x, int y) { this.x =x; this.y = y; }
+}
+
+class ColorPoint extends Point {
+    private String color;
+
+    public ColorPoint() {
+        super(0, 0);
+        this.color = "BLACK";
+    }
+
+    public ColorPoint(int x, int y) {
+        super(x, y);
+        this.color = "BLACK";
+    }
+
+    public void setXY(int x, int y) {
+        move(x, y);
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return color + "색의 (" + getX() + "," + getY() + ")의 점";
+    }
+}
+ class Main {
+    public static void main(String[] args) {
+        ColorPoint zeroPoint = new ColorPoint();
+        System.out.println(zeroPoint.toString() + "입니다.");
+        ColorPoint cp = new ColorPoint(10, 10);
+        cp.setXY(5, 5);
+        cp.setColor("RED");
+        System.out.println(cp.toString() + "입니다.");
+    }
+}
