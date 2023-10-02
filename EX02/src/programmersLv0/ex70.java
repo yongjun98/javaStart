97f8d9a7e8c5e7489fd517de393818365759c264
yongjun98 <116868167+yongjun98@users.noreply.github.com>
2023-10-02 18:46:36 +0900

    1
    1

--- EX02/src/programmersLv0/ex70.java
+++ EX02/src/programmersLv0/ex70.java
@@ -4,12 +4,22 @@ import java.util.Arrays;
 
 public class ex70 {
     public static void main(String[] args) {
-        int[]num_list = {2,1,6};
-        int n = 3;
-        System.out.println(Arrays.toString(solution(num_list, n)));
+        int a = 6;
+        int b = 1;
+        System.out.println(solution(a,b));
     }
 
-    private static int[] solution(int[] num_list, int n) {
-        
+    private static int solution(int a, int b) {
+        int answer = 0;
+        if(a % 2 ==1 && b % 2 == 1){
+            answer = a*a+b*b;
+        }
+        else if((a%2)+(b%2) ==1){
+            answer = 2*(a+b);
+        }
+        else{
+            answer = Math.abs(a-b);
+        }
+        return answer;
     }
 }
