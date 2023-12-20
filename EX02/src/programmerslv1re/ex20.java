db772bc21adaed560d7428cf3283d1dcb2ed38d2
yongjun98 <116868167+yongjun98@users.noreply.github.com>
2023-12-20 21:48:33 +0900

    1
    1

---
+++ EX02/src/programmerslv1re/ex20.java
@@ -0,0 +1,24 @@
+package programmerslv1re;
+
+public interface ex20 {
+    public static void main(String[] args) {
+        String phone_number = "01033334444";
+        System.out.println(solution(phone_number));
+    }
+
+    static String solution(String phone_number) {
+        String answer = "";
+        String[] num = phone_number.split("");
+
+        int len = num.length;
+
+        for(int i = 0; i < len; i++) {
+            if(i < len - 4)
+                answer += "*";
+            else
+                answer += num[i];
+        }
+
+        return answer;
+    }
+}
---
+++ EX02/src/programmerslv1re/ex22.java
@@ -0,0 +1,22 @@
+package programmerslv1re;
+
+public class ex22 {
+    public static void main(String[] args) {
+        String s = "abcde";
+        System.out.println(solution(s));
+    }
+
+    private static String solution(String s) {
+        String answer = "";
+        int length = s.length();
+
+        if(length % 2 == 0) {
+            answer = s.substring(length/2-1, length/2+1);
+        } else {
+            answer = s.substring(length/2, length/2+1);
+        }
+
+
+        return answer;
+    }
+}
